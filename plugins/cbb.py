982d228382e55fddf352a968617a7d54b2b7c954
Kai <131925584+Kaisan5@users.noreply.github.com>
2025-02-03 07:20:37 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -1,6 +1,6 @@
 
 from pyrogram import __version__
-from levi import Bot
+from bot import Bot
 from config import OWNER_ID
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 
