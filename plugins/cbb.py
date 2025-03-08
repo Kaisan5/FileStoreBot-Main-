c97f51a16b9150e42bca3e9f94266613caa5300f
Kai <131925584+Kaisan5@users.noreply.github.com>
2025-03-08 16:34:46 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -9,7 +9,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<blockquote><b>○ 𝐍𝐞𝐭𝐰𝐨𝐫𝐤 : <a href='https://t.me/AnimeNexusNetwork'>𝐀𝐧𝐢𝐦𝐞 𝐍𝐞𝐱𝐮𝐬</a>\n○ 𝐀ɴɪᴍᴇ 𝐂ʜᴀɴɴᴇʟ : <a href='https://t.me/Anime_Eternals'>𝐀𝐧𝐢𝐦𝐞 𝐄𝐭𝐞𝐫𝐧𝐚𝐥𝐬</a>\n○ 𝐎ɴɢᴏɪɴɢ 𝐂ʜᴀɴɴᴇʟ : <a href='https://t.me/Anime_Ongoing_Airings'>𝐎𝐧𝐠𝐨𝐢𝐧𝐠 𝐀𝐢𝐫𝐢𝐧𝐠𝐬</a>\n○ 𝐀ɴɪᴍᴇ 𝐂ʜᴀᴛ : <a href='https://t.me/Stelleron_Hunter'>𝐒𝐭𝐞𝐥𝐥𝐞𝐫𝐨𝐧 𝐇𝐮𝐧𝐭𝐞𝐫</a></b></blockquote>",
+            text = f"<blockquote><b>○ 𝐍𝐞𝐭𝐰𝐨𝐫𝐤 : <a href='https://t.me/AnimeNexusNetwork'>𝐀𝐧𝐢𝐦𝐞 𝐍𝐞𝐱𝐮𝐬</a>\n○ 𝐀ɴɪᴍᴇ 𝐂ʜᴀɴɴᴇʟ : <a href='https://t.me/Anime_Eternals'>𝐀𝐧𝐢𝐦𝐞 𝐄𝐭𝐞𝐫𝐧𝐚𝐥𝐬</a>\n○ 𝐎ɴɢᴏɪɴɢ 𝐂ʜᴀɴɴᴇʟ : <a href='https://t.me/+VxWwaMA6g_JkNTA9'>𝐎𝐧𝐠𝐨𝐢𝐧𝐠 𝐀𝐢𝐫𝐢𝐧𝐠𝐬</a>\n○ 𝐀ɴɪᴍᴇ 𝐂ʜᴀᴛ : <a href='https://t.me/Stelleron_Hunter'>𝐒𝐭𝐞𝐥𝐥𝐞𝐫𝐨𝐧 𝐇𝐮𝐧𝐭𝐞𝐫</a></b></blockquote>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
